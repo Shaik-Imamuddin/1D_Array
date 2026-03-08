b066f66e498e544a35fb610d2e4a4aece6cb3ed1
Immu <shaikimam37302@gmail.com>
2026-03-08 09:14:25 +0530

    Array Problem Solving

---
+++ Q08/main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int start=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]<0){
+            int temp=arr[i];
+            for(int j=i;j>start;j--){
+                arr[j]=arr[j-1];
+            }
+            arr[start]=temp;
+            start++;
+        }
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
