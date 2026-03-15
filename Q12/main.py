894a4c0d97d18f10c0fd0021d5c5f3821b05bada
Immu <shaikimam37302@gmail.com>
2026-03-15 12:53:19 +0530

    Array Problem Solving

---
+++ Q12/main.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=[]
+
+for i in range(n):
+    arr.append(int(input()))
+
+for i in range(n):
+    for j in range(i+1,n):
+        if arr[i]==arr[j]:
+            print("True")
+            exit()
+
+print("False")
