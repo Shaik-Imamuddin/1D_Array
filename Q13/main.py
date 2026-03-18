e0c7367396d4eaf7519bb294ff42f7af61e577a1
Immu <shaikimam37302@gmail.com>
2026-03-18 06:12:56 +0530

    Array Problem Solving

---
+++ Q13/main.py
@@ -0,0 +1,17 @@
+n=int(input())
+arr=[]
+
+for i in range(n):
+    arr.append(int(input()))
+
+start=0
+for i in range(n):
+    if arr[i]%2==0:
+        temp=arr[i]
+        for j in range(i,start,-1):
+            arr[j]=arr[j-1]
+        arr[start]=temp
+        start+=1
+
+for i in range(n):
+    print(arr[i],end=" ")
