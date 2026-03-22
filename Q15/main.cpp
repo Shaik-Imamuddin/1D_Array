60a13fdb117e1c5462ee8b1d6cc4bb9293837669
Immu <shaikimam37302@gmail.com>
2026-03-22 08:29:52 +0530

    Array Problem Solving

---
+++ Q15/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n-1;i++){
+        if(arr[i]<arr[i+1]){
+            cout<<"Not sorted in Descending Order";
+            return 0;
+        }
+    }
+    cout<<"Sorted in Descending Order";
+    return 0;
+}
