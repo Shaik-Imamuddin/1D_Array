a12c81914f66556e7679b6831472ae8e7598cb25
Immu <shaikimam37302@gmail.com>
2026-06-19 12:43:46 +0530

    Array Problem Solving

---
+++ Q37/main.py
@@ -0,0 +1,22 @@
+def reverse(arr,start,end):
+    while start<end:
+        arr[start],arr[end] = arr[end],arr[start]
+        start+=1
+        end-=1
+
+n = int(input())
+
+arr=[]
+
+for i in range(n):
+    arr.append(int(input()))
+
+k = int(input())
+k=k%n
+
+reverse(arr,0,k-1)
+reverse(arr,k,n-1)
+reverse(arr,0,n-1)
+
+for i in range(n):
+    print(arr[i],end=" ")
