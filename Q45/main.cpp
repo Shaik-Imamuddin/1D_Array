80dda93873ddb5b487fdf8c964487152cc6f1c0b
Immu <shaikimam37302@gmail.com>
2026-07-20 09:08:50 +0530

    Array Problem Solving

---
+++ Q45/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    for(int i=0;i<n/2;i++){
+        if(arr[i]!=arr[n-i-1]){
+            printf("Not a palindrome");
+            return 0;
+        }
+    }
+    printf("Palindrome");
+    return 0;
+}
